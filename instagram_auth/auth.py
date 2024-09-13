54e9adf8cbfb4a7a9180ea35ec7b0a1e4acccc42
MONKEY SPIN <166596134+MonkeySp1n@users.noreply.github.com>
2024-09-13 17:51:48 +0900

    auth.py 업데이트

--- instagram_auth/auth.py
+++ instagram_auth/auth.py
@@ -1,12 +1,12 @@
 import requests
 import time
 
-username = YOURT_USERNAME
+username = YOUR_USERNAME
 password = YOUR_PASSWORD
 
 session = requests.Session()
 session.get('https://www.instagram.com/')
-enpw = f"#PWD_INSTAGRAM_BROWSER:0:{int(time.time())}:{password}"
+enpw = f'#PWD_INSTAGRAM_BROWSER:0:{int(time.time())}:{password}'
 csrf = session.cookies['csrftoken']
 
 res = session.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/', 
